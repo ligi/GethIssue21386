1bc64ef66b627ce0a537d45b8d2b91b72bb4740a
ligi <ligi@ligi.de>
2020-08-11 12:14:48 +0200

    Initial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.websocket.WebSocketService
+
+fun main() {
+
+    val socketService = WebSocketService("wss://mainnet.infura.io/ws/v3/<infuraID>", true);
+
+    println("Started >>>>>>> ")
+
+    val web3jSocket = Web3j.build(socketService);
+    socketService.connect();
+
+    web3jSocket.transactionFlowable().subscribe { tx ->
+        println("Tx Hash > " +tx.hash)
+    }
+
+}
+
